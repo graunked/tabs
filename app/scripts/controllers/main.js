da5702d749fd2b711209c70f3eed10a17bdc23bc
David Graunke <graunked@gmail.com>
2014-10-17 15:34:12 -0700

    Initial commit

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,23 +1,62 @@
 'use strict';
 
 angular.module('scrollviewTestApp')
-  .controller('MainCtrl', function ($scope, $famous) {
+  .controller('MainCtrl', function ($scope, $famous, $timeline) {
 
     var EventHandler = $famous["famous/core/EventHandler"];
+    var Transitionable = $famous["famous/transitions/Transitionable"];
+    var TouchSync = $famous["famous/inputs/TouchSync"];
+
+    $scope.tabs = ["mountains", "lake", "shore"];
 
     $scope.scrollEvents = new EventHandler();
 
-    var playWiggle = function() {
-      $scope.play = false;
-      $scope.$digest();
-      $scope.play = true;
-      $scope.$digest();
+    $scope.width = function() {
+      return window.innerWidth * 5;
     };
 
-    document.addEventListener("keypress", function(e) {
-      if (e.which === 13) {
-        playWiggle();
-      }
+    var sync = new TouchSync();
+    $scope.scrollEvents.pipe(sync);
+
+    sync.on("update", function(data) {
+      console.log(position.get());
+      position.set(Math.min(Math.max(0, position.get() - data.delta[0] / 200), 2));
     });
+
+    sync.on("end", function(data) {
+      console.log(position.get());
+      position.set(Math.round(position.get()), {duration: 100});
+    });
+
+    var position = new Transitionable(0);
+
+    $scope.goTo = function(i) {
+      position.set(i, {duration: 200, transition: "inOutQuartic"});
+    };
+
+    $scope.timeline = function(i) {
+      return  i - position.get();
+    };
+
+    $scope.xTranslate = function(t) {
+      return t * 40 - 100;
+    };
+
+    $scope.zTranslate = $timeline([
+        [-1, -100],
+        [0, 0],
+        [1, -100],
+    ]);
+
+    $scope.opacity = $timeline([
+      [0, 1],
+      [1, 0]
+    ]);
+
+    $scope.tabOpacity = $timeline([
+      [-1, 0.5],
+      [0, 0],
+      [1, 0.5]
+    ]);
 
   });
